f76d073896d7258b4270c2b79991d78366e23d1d
TJN <142109365+tjn20@users.noreply.github.com>
2023-12-09 16:05:05 +0400

    Update MultiAcessKeyListInterface.java

--- MultiAcessKeyListInterface.java
+++ MultiAcessKeyListInterface.java
@@ -1,12 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
- */
-package com.mycompany.project;
 
 /**
  *
- * @author tjnla
+ * @author tjn20
  */
 public interface MultiAcessKeyListInterface <K,V> {
 
